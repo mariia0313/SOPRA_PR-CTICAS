4e0142e94e26ea8cda4d5cc3844d956620ec6202
Marc Nacher <marnacviv@alu.edu.gva.es>
2026-05-21 13:31:04 +0200

    HU-05

--- src/main/java/org/sopra/rogueguild/repository/model/ItemCategory.java
+++ src/main/java/org/sopra/rogueguild/repository/model/ItemCategory.java
@@ -1,10 +1,22 @@
 package org.sopra.rogueguild.repository.model;
 
+import java.util.Random;
+
 public enum ItemCategory {
-    WEAPON,
-    ARMOR,
-    POTION,
-    HELMET,
-    BOOTS,
-    OTHERS
+    WEAPON(new String[]{"Espada", "Hacha", "Daga", "Lanza", "Mandoble", "Arco", "Maza", "Bastón"}),
+    ARMOR(new String[]{"Armadura", "Cota", "Peto", "Coraza", "Malla"}),
+    BOOTS(new String[]{"Botas", "Grebas", "Sandalias", "Escarpines"}),
+    HELMET(new String[]{"Yelmo", "Casco", "Celada", "Capucha", "Visera"}),
+    POTION(new String[]{"Poción", "Elixir", "Brewaje", "Ungüento", "Tintura"});
+
+    private final String[] prefixes;
+    private final Random random = new Random();
+
+    ItemCategory(String[] prefixes) {
+        this.prefixes = prefixes;
+    }
+
+    public String getRandomPrefix() {
+        return prefixes[random.nextInt(prefixes.length)];
+    }
 }
