bca78b15d616312794bcc3ecbb25e6c7e633a532
Marc Nacher <marnacviv@alu.edu.gva.es>
2026-05-25 10:38:54 +0200

    HU-11

--- src/main/java/org/sopra/rogueguild/repository/model/Quest.java
+++ src/main/java/org/sopra/rogueguild/repository/model/Quest.java
@@ -4,22 +4,35 @@ import java.util.ArrayList;
 
 public class Quest {
 
+    private static Quests quests;
     private String description;
     private int goldReward;
-    private ArrayList<Item> requiredItems;
+    private ArrayList<ItemCategory> requiredItems;
     private boolean isCompleted;
 
-    public Quest(String description, int goldReward, ArrayList<Item> requiredItems){
+    public Quest(String description, int goldReward, ArrayList<ItemCategory> requiredItems){
         this.description = description;
         this.goldReward = (int) (Math.round(goldReward / 5.0) * 5);
         this.requiredItems = requiredItems;
         isCompleted = false;
+        quests.addQuest(this);
     }
 
 
     public boolean checkRequirement(Player player){
         boolean questRequirements = false;
-        if(player.getInventory().containsAll(requiredItems)){
+        boolean isMatched = false;
+        int itemsMatched = 0;
+        for (Item item : player.getInventory()) {
+            isMatched = false;
+            for(ItemCategory category : this.requiredItems){
+                if(item.getItemCategory().equals(category) && isMatched ){
+                    isMatched = true;
+                    itemsMatched++;
+                }
+            }
+        }
+        if(itemsMatched == requiredItems.size()){
             questRequirements = true;
         }
         return questRequirements;
@@ -28,5 +41,15 @@ public class Quest {
     public void completeQuest(){
         this.isCompleted = true;
     }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public boolean getStatus(){
+        return this.isCompleted;
+    }
+
+
 
 }
